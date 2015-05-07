9018093933e7f8d04ad5d7f753651e3c77c0cf12
Soeren <soeren@siegismund-poschmann.de>
2015-05-07 13:12:53 +0200

    Fix Nano (m() stuff and add nick name

--- pushbullet.py
+++ pushbullet.py
@@ -13,13 +13,10 @@ weechat.prnt("", "Hallo, von einem python Skript!")
 def notify_show(data, bufferp, uber_empty, tagsn, isdisplayed, 
 ishilight, prefix, message):
     if(ishilight):
-        buffer = (weechat.buffer_get_string(bufferp, "short_name") or 
-weechat.buffer_get_string(bufferp, "name"))
-        p.push_note('Weechat Hilight', buffer+": "+message)
-    elif(weechat.buffer_get_string(bufferp, "localvar_type") == 
-"private"):
-        buffer = (weechat.buffer_get_string(bufferp, "short_name") or 
-weechat.buffer_get_string(bufferp, "name"))
+        buffer = (weechat.buffer_get_string(bufferp, "short_name") or weechat.buffer_get_string(bufferp, "name"))
+         p.push_note('Weechat Hilight', buffer+": <"+prefix+"> "+message)
+    elif(weechat.buffer_get_string(bufferp, "localvar_type") == "private"):
+        buffer = (weechat.buffer_get_string(bufferp, "short_name") or weechat.buffer_get_string(bufferp, "name"))
         p.push_note('Weechat MSG from '+buffer, buffer+": "+message)
     return weechat.WEECHAT_RC_OK
 
