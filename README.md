76f7def58facbbcc79beb07cbde70a3e3c714b5d
Bharat Kunwar <b.kunwar@gmail.com>
2019-03-26 11:15:25 +0000

    Fix pushbullet.py, add requirements.txt and update README.md

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 Installation:
-====
+=============
 
-* Run ``pip install requests`` and ``pip install yapbl``
-* Download the pushbullet.py and put it in the python Plugin folder from
-Weechat ~/.weechat/python
-* Open the pushbullet.py and add your API-Key in the Script
+- Run:
 
-Run
-====
-Open Weechat and run ``\python load puschbullet.py`` and you will get notifications
+    pip install -r requirements.txt --user
+    cp pushpullet.py ~/.weechat/python/
+
+- Open `~/.weechat/python/pushbullet.py` and add your API-Key in the Script
+  which you can request here: https://www.pushbullet.com/#settings/account.
+
+- Open Weechat and run `/python load puschbullet.py` and you will get notifications.
--- pushbullet.py
+++ pushbullet.py
@@ -7,12 +7,10 @@ p = PushBullet(apikey)
 
 weechat.register("pushbullet", "kekskurse", "1.0", "MIT", "Send Pushbullet Notitifactions for hilights and private Messages in Weechat", "", "")
 
-
-def notify_show(data, bufferp, uber_empty, tagsn, isdisplayed, 
-ishilight, prefix, message):
+def notify_show(data, bufferp, uber_empty, tagsn, isdisplayed, ishilight, prefix, message):
     if(ishilight):
         buffer = (weechat.buffer_get_string(bufferp, "short_name") or weechat.buffer_get_string(bufferp, "name"))
-         p.push_note('Weechat Hilight', buffer+": <"+prefix+"> "+message)
+        p.push_note('Weechat Hilight', buffer+": <"+prefix+"> "+message)
     elif(weechat.buffer_get_string(bufferp, "localvar_type") == "private"):
         buffer = (weechat.buffer_get_string(bufferp, "short_name") or weechat.buffer_get_string(bufferp, "name"))
         p.push_note('Weechat MSG from '+buffer, buffer+": "+message)
